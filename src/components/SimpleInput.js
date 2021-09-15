b2c9a7ea670d37aedae1f005a8deb455f8adfcfd
Benjamin Austin <bnjmn80@gmail.com>
2021-09-15 13:22:07 -0600

    Remove useRef code, as it's unnecessary now

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,9 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const nameInputRef = useRef();
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -27,9 +26,6 @@ const SimpleInput = (props) => {
     event.preventDefault();
     console.log(enteredName);
 
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-
     setEnteredNameTouched(true);
 
     if (enteredName.trim() === '') {
@@ -50,7 +46,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input 
-          ref={nameInputRef} 
           type='text' 
           id='name' 
           value={enteredName} 
